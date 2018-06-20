a44cc12c653c4a7db0de63e56045eb8b7135573e
lluis santamaria <luis.santamaria.sola@gmail.com>
2018-06-20 17:36:19 +0200

    Initial commit. Reactive backend + Angular frontend

---
+++ cars-backend-reactive/src/main/java/com/demoprojects/springboot/carsbackendreactive/CarsBackendReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.demoprojects.springboot.carsbackendreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsBackendReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsBackendReactiveApplication.class, args);
+    }
+}
---
+++ src/test/java/com/demoprojects/springboot/carsproject/CarsProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demoprojects.springboot.carsproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarsProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
