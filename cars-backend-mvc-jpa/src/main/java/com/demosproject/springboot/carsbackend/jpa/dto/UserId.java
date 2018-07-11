1c22aed948a83bdfa54808c5fbe170d99d14d74d
lluis santamaria <luis.santamaria.sola@gmail.com>
2018-07-11 17:08:26 +0200

    Added operation for adding users to a current race

---
+++ cars-backend-mvc-jpa/src/main/java/com/demosproject/springboot/carsbackend/jpa/dto/UserId.java
@@ -0,0 +1,13 @@
+package com.demosproject.springboot.carsbackend.jpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserId {
+
+  private String id;
+}
